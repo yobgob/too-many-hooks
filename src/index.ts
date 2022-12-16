d0c8b9736752d98778a853f306daf8dda17a90a2
Dawson Booth <dawson@dawsonbooth.com>
2022-12-16 15:23:26 -0600

    Switch to node modules linker

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import { useState } from 'react'
 
 const useCount = (initial: number = 0) => {
   const [count, setCount] = useState<number>(initial)
-
   return { count, increment: () => setCount(c => c + 1) }
 }
 
-export { useCount as useTest }
+export { useCount }
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,22 @@
-import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-
+import dts from 'vite-plugin-dts'
 export default defineConfig({
-  plugins: [react()],
+  appType: 'custom',
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: 'src/index.ts',
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'too-many-hooks',
-      // the proper extensions will be added
       fileName: 'too-many-hooks',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['react'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           react: 'React',
         },
       },
     },
   },
+  plugins: [dts()],
 })
