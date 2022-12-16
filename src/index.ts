da70ea1073e198ff91b55ea46f3d7c79924a338d
Dawson Booth <dawson@dawsonbooth.com>
2022-12-16 15:51:55 -0600

    Move package to subfolder

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-const useCount = (initial: number = 0) => {
-  const [count, setCount] = useState<number>(initial)
-  return { count, increment: () => setCount(c => c + 1) }
-}
-
-export { useCount }
---
+++ too-many-hooks/src/index.ts
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+
+const useCount = (initial: number = 0) => {
+  const [count, setCount] = useState<number>(initial)
+  return { count, increment: () => setCount(c => c + 1) }
+}
+
+export { useCount }
