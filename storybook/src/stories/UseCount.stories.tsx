898ea34a38b73e28c6bc10526ee40e7e719ff692
Patrick DeVries <pdevries@headstorm.com>
2022-12-19 23:50:52 -0600

    Add UseCount story

---
+++ storybook/src/stories/UseCount.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { userEvent, within } from '@storybook/testing-library'
+import dedent from 'ts-dedent'
+
+import { UseCount } from './UseCount'
+
+const DESCRIPTION = dedent`
+A demo of the \`useCount\` hook. The \`onClick\` control determines whether to call \`increment\` or \`decrement\` on click.
+<br/><br/>\`count: number\` is the current count, displayed on the button
+<br/>\`increment: () => void\` adds one to the current count
+<br/>\`decrement: () => void\` subtracts one from the current count
+<br/><br/>Example hook usage:
+
+\`\`\`tsx
+import { useCount } from 'too-many-hooks'
+
+const { count, increment, decrement } = useCount()
+
+<button onClick={onClick === 'increment' ? increment : decrement}>
+  Clicked {count} time{count === 1 ? '' : 's'}
+</button>
+\`\`\``
+
+const meta = {
+  title: 'Example/UseCount',
+  component: UseCount,
+  parameters: {
+    layout: 'centered',
+    controls: { expanded: true },
+    docs: {
+      title: 'useCount',
+      description: {
+        component: DESCRIPTION,
+      },
+    },
+  },
+  tags: ['docsPage'],
+  argTypes: {
+    onClick: {
+      control: {
+        type: 'radio',
+      },
+      options: ['increment', 'decrement'],
+      defaultValue: 'increment',
+    },
+  },
+} satisfies Meta<typeof UseCount>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const UseCountStory: Story = {
+  play: ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const countingButton = canvas.getByRole('button')
+    userEvent.click(countingButton)
+  },
+  args: {
+    onClick: 'increment',
+  },
+}
