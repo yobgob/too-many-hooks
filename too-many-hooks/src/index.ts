36dd871ea3ef655b786c672aba9d60038b7baff2
Patrick DeVries <pdevries@headstorm.com>
2022-12-19 23:49:07 -0600

    Add decrement function to useCount

--- too-many-hooks/src/index.ts
+++ too-many-hooks/src/index.ts
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const useCount = (initial: number = 0) => {
   const [count, setCount] = useState<number>(initial)
-  return { count, increment: () => setCount(c => c + 1) }
+  return { count, increment: () => setCount(c => c + 1), decrement: () => setCount(c => c - 1) }
 }
 
 export { useCount }
