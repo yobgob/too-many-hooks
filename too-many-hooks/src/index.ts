79f0e9ac3aef365ce35300abc9bd500a93795c4c
Patrick DeVries <pdevries@headstorm.com>
2022-12-21 22:52:42 -0600

    Add step and doc comments to useCount

--- too-many-hooks/src/index.ts
+++ too-many-hooks/src/index.ts
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-
-const useCount = (initial: number = 0) => {
-  const [count, setCount] = useState<number>(initial)
-  return { count, increment: () => setCount(c => c + 1), decrement: () => setCount(c => c - 1) }
-}
+import useCount from './useCount'
 
 export { useCount }
---
+++ too-many-hooks/src/useCount.ts
@@ -0,0 +1,32 @@
+import { useCallback, useState } from 'react'
+
+interface Args {
+  /** Initial `count`, defaults to `0` */
+  initial?: number
+  /** Amount to increment or decrement by, defaults to `1` */
+  step?: number
+}
+
+/** Returns a numeric `count` and functions to increment or decrement it */
+const useCount = ({ initial = 0, step = 1 }: Args) => {
+  const [count, setCount] = useState<number>(initial)
+
+  const increment = useCallback(
+    (stepOverride?: number) => setCount(c => c + (stepOverride ?? step)),
+    [step],
+  )
+  const decrement = useCallback(
+    (stepOverride?: number) => setCount(c => c - (stepOverride ?? step)),
+    [step],
+  )
+
+  return {
+    count,
+    /** Adds the `stepOverride` or, if `undefined`, `step` to `count` */
+    increment,
+    /** Subtracts the `stepOverride` or, if `undefined`, `step` from `count` */
+    decrement,
+  } as const
+}
+
+export default useCount
