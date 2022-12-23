24f4bde8a61eece3c51c649f4a0fc80c0ef0c86f
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2022-12-23 13:37:37 -0600

    Simplify verbiage
    Co-authored-by: Dawson Booth <dawson@dawsonbooth.com>

--- storybook/src/stories/UseFlag/Modal.tsx
+++ storybook/src/stories/UseFlag/Modal.tsx
@@ -12,7 +12,7 @@ const Modal: React.FC = () => {
       <Modal_ isOpen={isOpen} onClose={close}>
         <div className="leading-8">
           This modal was opened with a simple<code>onClick=&#123;open&#125;</code>. Clicking
-          <code>&#10005;</code> or outside of the modal will close it with a simple
+          <code>&#10005;</code> or outside of the modal will close it via
           <code>onClick=&#123;close&#125;</code>. No setters needed.
         </div>
       </Modal_>
