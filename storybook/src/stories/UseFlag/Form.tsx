66c105e01bb25c79bfe6fd5f19770de1aa130510
Patrick DeVries <pdevries@headstorm.com>
2022-12-23 13:18:52 -0600

    Anonymise email

--- storybook/src/stories/UseFlag/Form.tsx
+++ storybook/src/stories/UseFlag/Form.tsx
@@ -14,7 +14,7 @@ const Form: React.FC = () => {
           <input
             type="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
+            placeholder="name@example.com"
             onChange={() => {
               setUnsaved()
               setHasBegun()
