9df744126c3536b3175b7c599b5e1a0702c3af57
Dawson Booth <dawson@dawsonbooth.com>
2023-01-04 08:18:47 -0600

    Add PR lint and build checks (#16)

--- storybook/.eslintrc.cjs
+++ storybook/.eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
   ],
