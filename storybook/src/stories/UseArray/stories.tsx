5caf778b0c7a28dfc26bf4326b4e4ea2603a2b07
Patrick DeVries <pdevries@headstorm.com>
2023-01-14 23:53:14 -0600

    Update docs to new standard

--- storybook/src/stories/UseArray/stories.tsx
+++ storybook/src/stories/UseArray/stories.tsx
@@ -2,21 +2,12 @@ import type { Meta, StoryObj } from '@storybook/react'
 import Burger from './Burger'
 import BURGER_CODE from './Burger.tsx?raw'
 
-const meta = {
-  title: 'Example/UseArray',
-  tags: ['docsPage'],
-} satisfies Meta
-
-export default meta
-
-export const BurgerStory: StoryObj<Meta<typeof Burger>> = {
-  name: 'Burger',
+export const burger: StoryObj<Meta<typeof Burger>> = {
   parameters: {
-    component: Burger,
     layout: 'fullscreen',
     docs: {
       source: { code: BURGER_CODE, language: 'tsx' },
     },
   },
-  render: () => <Burger />,
+  render: args => <Burger {...args} />,
 }
