c9834bc601e4ccb066f579f0cb78215e42c61974
Patrick DeVries <pdevries@headstorm.com>
2023-01-25 17:25:30 -0600

    Remove out of scope hooks

--- too-many-hooks/src/index.ts
+++ too-many-hooks/src/index.ts
@@ -1,6 +1,5 @@
 import useArray, { UseArray, UseArrayReturn } from './useArray'
 import useCount, { UseCount, UseCountArgs, UseCountReturn } from './useCount'
-import useDeepCompareEffect, { UseDeepCompareEffect } from './useDeepCompareEffect'
 import useFlag, { UseFlag, UseFlagReturn } from './useFlag'
 import useFlexCorners, {
   Corner,
@@ -9,7 +8,6 @@ import useFlexCorners, {
   UseFlexCornersReturn,
 } from './useFlexCorners'
 import useMutationObserver, { UseMutationObserver } from './useMutationObserver'
-import useResizeObserver, { UseResizeObserver } from './useResizeObserver'
 import useWindowListener, { UseWindowListener, UseWindowListenerOptions } from './useWindowListener'
 
 export type {
@@ -20,23 +18,12 @@ export type {
   UseCount,
   UseCountArgs,
   UseCountReturn,
-  UseDeepCompareEffect,
   UseFlag,
   UseFlagReturn,
   UseFlexCorners,
   UseFlexCornersReturn,
   UseMutationObserver,
-  UseResizeObserver,
   UseWindowListener,
   UseWindowListenerOptions,
 }
-export {
-  useArray,
-  useCount,
-  useDeepCompareEffect,
-  useFlag,
-  useFlexCorners,
-  useMutationObserver,
-  useResizeObserver,
-  useWindowListener,
-}
+export { useArray, useCount, useFlag, useFlexCorners, useMutationObserver, useWindowListener }
