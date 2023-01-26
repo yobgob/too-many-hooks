381bb937a2527ad7336572e59225c8e15bb8da30
Dawson Booth <dawson@dawsonbooth.com>
2023-01-25 22:40:23 -0600

    Fix issues from merge

--- storybook/src/stories/UseTally/SocialMedia.tsx
+++ storybook/src/stories/UseTally/SocialMedia.tsx
@@ -21,7 +21,7 @@ const SocialMedia: React.FC<UseTallyStoryProps> = ({ step, incrementStep, decrem
       <div className="ml-3.5 border-l border-slate-400 pl-3.5">
         <p className="m-1">
           This new library too-many-hooks has huge potential. I hope they add a hook I can use to
-          easily count likes/dislikes on a post. Maybe a &quot;useTally&quot;?
+          easily tally likes/dislikes on a post. Maybe a &quot;useTally&quot;?
         </p>
       </div>
       <div className="flex items-baseline gap-2 pl-3.5">
