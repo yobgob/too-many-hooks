e7f1b2f73366503c772cb8585d11033f3fd76153
Dawson Booth <dawson@dawsonbooth.com>
2023-08-28 19:44:43 -0500

    Run linting against entire project

--- packages/storybook/README.md
+++ packages/storybook/README.md
@@ -6,11 +6,11 @@ This directory contains the storybook application for testing and demoing `too-m
 
 Install / update dependencies with:
 
-```yarn install```
+`yarn install`
 
 Then, run the storybook at [http://localhost:6006/](http://localhost:6006/) with:
 
-```yarn storybook```
+`yarn storybook`
 
 ## Tooling
 
--- packages/storybook/src/stories/UseFlag/Modal.tsx
+++ packages/storybook/src/stories/UseFlag/Modal.tsx
@@ -13,7 +13,8 @@ const Modal: React.FC = () => {
       </Button>
       <Modal_ isOpen={isOpen} onClose={close}>
         <div className="prose leading-8" data-testid="modal-text">
-          This modal was opened with a simple<code>onClick=&#123;open&#125;</code>. Clicking
+          This modal was opened with a simple
+          <code>onClick=&#123;open&#125;</code>. Clicking
           <code>&#10005;</code> or outside of the modal will close it via
           <code>onClick=&#123;close&#125;</code>. No setters needed.
         </div>
