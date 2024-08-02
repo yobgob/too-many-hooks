6cf8920a3594641f5ef718d967a24183ff2fc1a2
Dawson Booth <dawson@dawsonbooth.com>
2024-08-02 15:42:04 -0500

    Fix build and package issues (#105)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.eslint.json'],
+    project: ['./tsconfig.json'],
     extraFileExtensions: ['.mdx'],
     ecmaFeatures: {
       jsx: true,
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   appType: 'custom',
-  plugins: [dts({ insertTypesEntry: true }), react()],
+  plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: 'src/index.ts',
       name: 'too-many-hooks',
       fileName: 'too-many-hooks',
     },
+    emptyOutDir: false,
     rollupOptions: {
-      external: ['react', 'react-dom', '.storybook'],
+      external: ['react', 'react-dom', 'lodash-es'],
       output: {
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
+          'lodash-es': '_',
         },
       },
     },
