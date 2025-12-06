46d80a7c0c1cefeb4147b1be7a2158abf4964fb1
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2025-12-05 23:17:13 -0500

    Fix ignored files

--- run-on-changed.sh
+++ run-on-changed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-changed_files=$(git diff --relative -r --name-only --diff-filter=ACMRTUXB origin/main | grep -E '.*\.ts$|.*\.tsx$|.*\.js$|.*\.jsx$')
+changed_files=$(git diff --relative -r --name-only --diff-filter=ACMRTUXB origin/main | grep -E '.*\.ts$|.*\.tsx$|.*\.js$|.*\.jsx$' | grep -v -E '^dist/|^storybook-static/|\.config\.js$|^eslint\.config\.js$|^\.storybook/')
 
 if [ -z "$changed_files" ]; then
   exit 0
